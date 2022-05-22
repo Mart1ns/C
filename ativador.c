ed4db0da9e908139d4ebcfacffbb54ae02a98b6b
Mart1ns <pauloeduardomartins700305@gmail.com>
2022-05-22 16:49:52 -0300

    'Ativdador'

---
+++ ativador.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+   
+   
+   int dec, wind;
+
+    system("slmgr/dlv");
+    Sleep(750);
+    system("slmgr/upk");
+   
+    system("color 0a");
+    printf("   _                  \n");
+    printf("  | |                 \n");
+    system("color 1");
+    Sleep(250);
+    printf("  | | ___ __ ___  ____\n");
+    system("color 2");
+    Sleep(500);
+    printf("  | |/ / '_ ` _ ||_  /\n");
+    system("color 3");
+    Sleep(750);
+    printf("  |   <| | | | | |/ / \n");
+    system("color 4");
+    Sleep(750);
+    printf("  |_||_|_| |_| |_/___|\n");
+    Sleep(2000);
+    printf("\n");
+
+    system("cls");
+    system("color 6");
+    Sleep(250);
+    printf("******************************************************************\n");
+    system("color 3");
+    Sleep(250);
+    printf("**|A guerra cibernetica foi declarada a muito tempo.....|*********\n");
+    system("color 1");
+    Sleep(250);
+    printf("******************************************************************\n");
+    Sleep(3000);
+    system("cls");
+    Sleep(250);
+    printf("********************************\n");
+    system("color 6");
+    Sleep(250);
+    printf("**|Ativador de Windows|*********\n");
+    system("color 2");
+    Sleep(250);
+    printf("********************************\n");
+    Sleep(3000);
+    system("cls");
+    system("color 0a");
+    printf("--> Eae, ");
+    system("Whoami");
+    printf("\n");
+    printf("--> Irei exibir as especificacoes do seu sistema a seguir: ");
+    Sleep(7000);
+    system("cls");
+    system("color 4");
+    printf("\n");
+    system("systeminfo");
+    Sleep(7500);
+    system("cls");
+    system("color 0a");
+
+    printf("--> Deseja ativar o Windows agora? [ 1 = Sim | 2 = Nao ]\n--> ");
+    
+
+
+
+    
+
+
+    scanf("%d", &dec);
+    printf("\n");
+    if (dec == 1)
+    {   
+        system("cls");
+        printf("--> Caso apresente algum erro, execute em modo de administrador!\n-");
+        printf("--> Escolha a versao:\n\n |1 = Home/Core\n |2 = Home/Core (Country Specific)\n |3 = Home/Core (Single Language)\n |4 = Home/Core N\n |5 = Professional(Wind pro)\n |6 = Professional N(Wind pro N)\n |7 = Enterprise\n |8 = Enterprise N\n |9 = Education\n |10 = Education N\n\n--> ");
+        
+        scanf("%d", &wind);
+        switch(wind)
+        {
+        case 1:
+            system("cscript slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 ");
+            Sleep(250);
+            system("cscript slmgr.vbs /skms kms.lotro.cc");
+            Sleep(250);
+            system("cscript slmgr.vbs /ato");
+            Sleep(250);
+            printf("\n");
+            printf("--> Ativado com sucesso!");
+            Sleep(3000);
+            system("shutdown -r");
+            break;
+        case 2:
+            system("cscript slmgr.vbs /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR");
+            Sleep(250);
+            system("cscript slmgr.vbs /skms kms.lotro.cc");
+            Sleep(250);
+            system("cscript slmgr.vbs /ato");
+            Sleep(250);
+            printf("\n");
+            printf("--> Ativado com sucesso!");
+            Sleep(3000);
+            system("shutdown -r");
+            break;
+        case 3:
+            system("cscript slmgr.vbs /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH");
+            Sleep(250);
+            system("cscript slmgr.vbs /skms kms.lotro.cc ");
+            Sleep(250);
+            system("cscript slmgr.vbs /ato");
+            Sleep(250);
+            printf("\n");
+            printf("--> Ativado com sucesso!");
+            Sleep(3000);
+            system("shutdown -r");
+            break;
+        case 4:
+            system("cscript slmgr.vbs /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM");
+            Sleep(250);
+            system("cscript slmgr.vbs /skms kms.lotro.cc ");
+            Sleep(250);
+            system("cscript slmgr.vbs /ato");
+            Sleep(250);
+            printf("\n");
+            printf("--> Ativado com sucesso!");
+            Sleep(3000);
+            system("shutdown -r");
+            break;
+        case 5:
+            system("cscript slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX");
+            Sleep(250);
+            system("cscript slmgr.vbs /skms kms.lotro.cc ");
+            Sleep(250);
+            system("cscript slmgr.vbs /ato");
+            Sleep(250);
+            printf("\n");
+            printf("--> Ativado com sucesso!");
+            Sleep(3000);
+            system("shutdown -r");
+            break;
+        case 6:
+            system("cscript slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9");
+            Sleep(250);
+            system("cscript slmgr.vbs /skms kms.lotro.cc ");
+            Sleep(250);
+            system("cscript slmgr.vbs /ato");
+            Sleep(250);
+            printf("\n");
+            printf("--> Ativado com sucesso!");
+            Sleep(3000);
+            system("shutdown -r");
+            break;
+        case 7:
+            system("cscript slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43");
+            Sleep(250);
+            system("cscript slmgr.vbs /skms kms.lotro.cc ");
+            Sleep(250);
+            system("cscript slmgr.vbs /ato");
+            Sleep(250);
+            printf("\n");
+            printf("--> Ativado com sucesso!");
+            Sleep(3000);
+            system("shutdown -r");
+            break;
+        case 9:
+            system("cscript slmgr.vbs /ipk  DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4");
+            Sleep(250);
+            system("cscript slmgr.vbs /skms kms.lotro.cc ");
+            Sleep(250);
+            system("cscript slmgr.vbs /ato");
+            Sleep(250);
+            printf("\n");
+            printf("--> Ativado com sucesso!");
+            Sleep(3000);
+            system("shutdown -r");
+            break;
+        case 10:
+            system("cscript slmgr.vbs /ipk  NW6C2-QMPVW-D7KKK-3GKT6-VCFB2");
+            Sleep(250);
+            system("cscript slmgr.vbs /skms kms.lotro.cc ");
+            Sleep(250);
+            system("cscript slmgr.vbs /ato");
+            Sleep(250);
+            printf("\n");
+            printf("--> Ativado com sucesso!");
+            Sleep(3000);
+            system("shutdown -r");
+            break;
+        }
+        
+    }
+    else if (dec == 2)
+    {   
+        system("cls");
+        printf("Adeus, ");
+        system("whoami");
+         
+    }
+    
+    else {
+
+        printf("--> Opcao invalida!");
+    }
+  
+  return 0;
+}
